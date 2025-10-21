fa6e7ec5acd0cad526f8c1eac8b3c70208dd0379
Hasan Naqvi <hasannaqvi2007@icloud.com>
2025-10-21 13:40:05 -0400

    Bootcamp Part 1 done

--- modules/detect_colours.py
+++ modules/detect_colours.py
@@ -8,11 +8,6 @@ from pathlib import Path
 import cv2
 import numpy as np
 
-# Bootcampers remove the following lines:
-# Allow linters and formatters to pass for bootcamp maintainers
-# pylint: disable=unused-argument,unused-variable,used-before-assignment
-
-
 class DetectBlue:
     """
     Detects blue objects from an image.
@@ -51,6 +46,7 @@ class DetectBlue:
         # Convert the image's colour to HSV
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+
         # Set upper and lower bounds for colour detection, this is in HSV
         lower_blue = np.array([100, 150, 50])
         upper_blue = np.array([140, 255, 255])
@@ -58,8 +54,9 @@ class DetectBlue:
         # Apply the threshold for the colour detection
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
+
         # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img, img, mask=mask)
+        res = cv2.bitwise_and(img,img, mask= mask)
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -123,13 +120,14 @@ class DetectRed:
         lower_red2 = np.array([170, 70, 50])
         upper_red2 = np.array([180, 255, 255])
 
+
         # Apply the threshold for the colour detection
         mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
         mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
         mask = cv2.bitwise_or(mask1, mask2)
 
         # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img, img, mask=mask)
+        res = cv2.bitwise_and(img,img, mask= mask)
 
         # Annotate the colour detections
         # replace the '_' parameter with the appropiate variable
